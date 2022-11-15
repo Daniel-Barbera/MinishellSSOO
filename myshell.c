17795870a253f5d54988f523a307572a7982aa9a
Daniel-Barbera <dabarbera26@gmail.com>
2022-11-15 17:03:15 +0100

    Save before trying again from the start

--- myshell.c
+++ myshell.c
@@ -9,27 +9,32 @@
 #include <sys/types.h> // pid_t
 #include <sys/stat.h> 
 #include "parser.h"
+// Constant definitions
 #define BACKGROUND_JOBS_MAX 10
+#define WHITE "\033[0m"
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
+#define YELLOW "\x1b[33m"
+#define RED "\x1b[31m"
+#define RESET "\x1b[0m"
 
 // Function declarations
 // Shell prompt helpers
 void print_prompt();
-void print_green(char * str);
-void print_blue(char * str);
-void print_yellow(char * str);
-void print_red(char * str);
+void print_color(FILE * stream, char * color, char * string);
 char * polite_directory_format(char * name);
 // Signal handlers
 void sigint_handler(int sig);
 void sigchld_handler(int sig);
 // Shell helpers
 void parse_shell_args(int argc, char ** argv);
-char * parse_tcommand_to_string(tcommand * command, char * buffer);
-void execute_command(tcommand * command);
+void parse_tcommand_to_string(tcommand * command, char * buffer);
+void execute_commands(tcommand ** commands);
+void kill_job(pid_t pid);
 // Shell commands
 void cd(char * path);
-void umask(tcommand * command);
-void background(tcommand * command);
+void _umask(char * mask);
+void background(tcommand ** commands);
 void foreground(pid_t pid);
 void jobs();
 
@@ -44,7 +49,10 @@ pid_t current_pid;                           // PID of the current process being
 background_job background_jobs[BACKGROUND_JOBS_MAX];
 size_t background_jobs_idx = 0;              // Index of the next available position in background_jobs
 bool extended_support = false;               // Flag to indicate if other shell commands should be executed
-
+// Variables for output redirection
+FILE * input_file;                    // File to redirect input from
+FILE * output_file;                   // File to redirect output to
+FILE * error_file;                    // File to redirect error to
 
 int main(int argc, char *argv[]) {
   char input_buffer[PATH_MAX];
@@ -52,6 +60,9 @@ int main(int argc, char *argv[]) {
   tcommand * tcommands; 
   tcommand command;
 
+  input_file = stdin;
+  output_file = stdout;
+  error_file = stderr;
   parse_shell_args(argc, argv);
   signal(SIGINT, sigint_handler);
   signal(SIGCHLD, sigchld_handler);
@@ -62,17 +73,13 @@ int main(int argc, char *argv[]) {
       tcommands = tline->commands;
       if (tline->background) {
         for (int i = 0; i < tline->ncommands; i++) {
-          command = tcommands[i];
-          background(&command);
+          background(tcommands);
         }
       } else {
-        for (int i = 0; i < tline->ncommands; i++) {
-          command = tcommands[i];
-          execute_command(&command);
-        }
+        execute_commands(tcommands);
+        print_prompt();
       }
     }
-    print_prompt();
   }
   return 0;
 }
@@ -84,35 +91,18 @@ void print_prompt() {
 
   gethostname(hostname, sizeof(hostname));
   sprintf(prompt, "%s@%s", getenv("USER"), hostname);
-  print_green(prompt);
+  print_color(stdout, GREEN, prompt);
   printf(":");
   getcwd(prompt, sizeof(prompt));
-  print_blue(polite_directory_format(prompt));
+  print_color(stdout, BLUE, polite_directory_format(prompt));
   printf(" msh> ");
 }
-void print_green(char *str) {
-  printf("\033[1;32m");
-  printf("%s", str);
-  printf("\033[0m");
+void print_color(FILE * stream, char * color, char * string) {
+  fprintf(stream, "%s%s%s", color, string, RESET);
 }
-void print_blue(char *str) {
-  printf("\033[1;34m");
-  printf("%s", str);
-  printf("\033[0m");
-}
-void print_yellow(char *str) {
-  printf("\033[1;33m");
-  printf("%s", str);
-  printf("\033[0m");
-}
-void print_red(char *str) {
-  printf("\033[1;31m");
-  printf("%s", str);
-  printf("\033[0m");
-}
-// Del código fuente de Bash.
-// https://github.com/bminor/bash/blob/fb0092fb0e7bb3121d3b18881f72177bcb765491/general.c
 char * polite_directory_format(char * name) {
+  // Del código fuente de Bash.
+  // https://github.com/bminor/bash/blob/fb0092fb0e7bb3121d3b18881f72177bcb765491/general.c
   char * home = getenv("HOME");
   static char tdir[PATH_MAX];
   int length;
@@ -136,6 +126,7 @@ void sigint_handler(int sig) {
 void sigchld_handler(int sig) {
   int status;
   pid_t pid;
+
   while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
     if (WIFEXITED(status)) {
       printf("Process %d exited with status %d", pid, WEXITSTATUS(status));
@@ -147,53 +138,43 @@ void sigchld_handler(int sig) {
       printf("Process %d continued", pid);
     }
   }
-  
-  bool element_found = false;
-  for (size_t i = 0; i < background_jobs_idx && !element_found; i++) {
-    element_found = background_jobs[i]->pid == pid;
-    if (element_found) {
-      if (i == background_jobs_idx) {
-        background_jobs_idx--;
-      } else {
-        while (i < background_jobs_idx) {
-          background_jobs[i] = background_[i + 1];
-          i++;
-        }
-        background_jobs_idx--;
-      }
-    }
-  }
 }
 
 // Shell helpers
 void parse_shell_args(int argc, char ** argv) {
   if (argc > 1) {
-    for (int i = 1; i < argc; i++) {
+    for (size_t i = 1; i < argc; i++) {
       if (strcmp(argv[i], "-s") == 0) {
         extended_support = true;
       }
     }
   }
-  printf("Welcome to msh, the minimal shell. Type 'help' for more information.\n");
   if (!extended_support) {
-    print_yellow("WARNING: the shell has been initialized without the -s option.\n");
-    print_yellow("Only the commands 'cd', 'exit', 'fg', 'jobs' and 'umask' will be available.\n");
+    print_color(stdout, YELLOW, "WARNING: the shell has been initialized without the -s option.\n");
+    print_color(stdout, YELLOW, "Only the commands 'cd', 'exit', 'fg', 'jobs' and 'umask' will be available.\n");
   }
 }
-char * parse_tcommand_to_string(tcommand * command, char * buffer) {
+void parse_tcommand_to_string(tcommand * command, char * buffer) {
+  size_t length; 
+
+  length = strlen(command->filename + 1);
+  for (size_t i = 0; i < command->argc; ++i) {
+    length += strlen(command->argv[i]); + 1;
+  }
+  buffer = (char *) malloc(length * sizeof(char));
   strcpy(buffer, command->filename);
-  for (int i = 0; i < command->argc; i++) {
+  for (size_t i = 0; i < command->argc; i++) {
     strcat(buffer, " ");
     strcat(buffer, command->argv[i]);
   }
-  return buffer;
 }
 void execute_command(tcommand * command) {
   char error_message[FILENAME_MAX + 30];
+
   if (strcmp(command->filename, "cd") == 0) {
     cd(command->argv[1]);
   } else if (strcmp(command->filename, "umask") == 0) {
-    umask(command);
+    _umask(command->argv[0]);
   } else if (strcmp(command->filename, "jobs") == 0) {
     jobs();
   } else if (strcmp(command->filename, "fg") == 0) {
@@ -204,18 +185,29 @@ void execute_command(tcommand * command) {
     if (extended_support) {
       // try to execute the command as a linux command
       pid_t pid = fork();
-      if (pid == 0) {
-        execvp(command->filename, command->argv);
-        sprintf(error_message, "%s: command not found", command->filename);
-        perror(error_message);
-        exit(1);
-      } else {
-        waitpid(pid, NULL, 0);
-      }
     } else {
       sprintf(error_message, "msh: command not found: %s", command->filename);
-      print_yellow("Consider using the -s option to enable extended support.\n");
-      print_red(error_message);
+      print_color(error_file, YELLOW, "Consider using the -s option to enable extended support.\n");
+      print_color(error_file, RED, error_message);
+    }
+  }
+}
+
+void kill_job(pid_t pid) {
+  bool element_found = false;
+  for (size_t i = 0; i < background_jobs_idx && !element_found; i++) {
+    element_found = background_jobs[i].pid == pid;
+    if (element_found) {
+      free(background_jobs[i].command);
+      if (i == background_jobs_idx) {
+        background_jobs_idx--;
+      } else {
+        while (i < background_jobs_idx) {
+          background_jobs[i] = background_jobs[i + 1];
+          i++;
+        }
+        background_jobs_idx--;
+      }
     }
   }
 }
@@ -228,12 +220,26 @@ void cd(char * path) {
     chdir(path);
   }
 }
-void umask(tcommand * command) {
-  umask()
+
+void _umask (char * mask) {
+  mode_t mask_value;
+  char mask_string[4];
+
+  if (mask != NULL) {
+    mask_value = strtol(mask, NULL, 8);
+    umask(mask_value);
+    return;
+  } else {
+    mask_value = umask(022);
+    umask(mask_value);
+    sprintf(mask_string, "%03o", mask_value);
+    print_color(output_file, WHITE, mask_string);       
+  }
 }
-void background(tcommand * command) {
+
+void background(tcommand ** command) {
   if (background_jobs_idx + 1 == BACKGROUND_JOBS_MAX) {
-    print_red("Error: exceeded the maximum number of background jobs");
+    print_color(error_file, RED, "Error: exceeded the maximum number of background jobs");
     return;
   }
   pid_t pid = fork();
@@ -244,7 +250,8 @@ void background(tcommand * command) {
     exit(0);
   } else {
     // Parent process
-    background_pids[background_jobs_idx] = pid;
+    background_jobs[background_jobs_idx].pid = pid;
+    parse_tcommand_to_string(command, background_jobs[background_jobs_idx].command);
     ++background_jobs_idx;
   }
 }
@@ -254,6 +261,6 @@ void foreground(pid_t pid) {
 }
 void jobs() {
   for (size_t i = 0; i < background_jobs_idx; i++) {
-    printf("[%d] %d\n", i, background_pids[i]);
+    printf("[%d] %s\n", background_jobs[i].pid, background_jobs[i].command);
   }
 }
