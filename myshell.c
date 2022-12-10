a0043edce59ee97963e67f73b2b13a196b7f4553
Daniel-Barbera <dabarbera26@gmail.com>
2022-12-10 18:32:51 +0100

    fork, pipes in progress

--- myshell.c
+++ myshell.c
@@ -12,7 +12,7 @@
 #include "parser.h"
 // Constant definitions
 #define BACKGROUND_JOBS_MAX 10
-#define WHITE "\033[0m"
+#define WHITE "\x1b[0m"
 #define GREEN "\x1b[32m"
 #define BLUE "\x1b[34m"
 #define YELLOW "\x1b[33m"
@@ -36,8 +36,7 @@ void parse_shell_args(int argc, char ** argv);
 bool set_redirection_variables(tline * line);
 bool cd_or_exit_are_present(tline * line);
 void parse_tcommand_to_string(tcommand * command, char * buffer);
-void execute_commands(tcommand ** commands);
-background_job execute_commands_in_background(tcommand ** commands);
+void execute_commands(tcommand ** commands, bool background);
 void close_redirection_files();
 void kill_job(pid_t pid);
 
@@ -45,11 +44,11 @@ void kill_job(pid_t pid);
 
 void cd(char * path);
 void _umask(char * mask);
-void foreground(pid_t pid);
+void foreground(size_t job_id);
 void jobs();
 
 // Type definitions
-/** PID and string representation of a job */
+/** PID and string representation of a job. */
 typedef struct {
   pid_t pid;
   char * command;
@@ -66,36 +65,44 @@ FILE * error_file;                                   // File to redirect error t
 
 int main(int argc, char *argv[]) {
   char input_buffer[PATH_MAX];
+  char * bad_command;
   tline * line;
-  // Me gusta la gasolina
+
   parse_shell_args(argc, argv);
   signal(SIGINT, sigint_handler);
   signal(SIGCHLD, sigchld_handler);
   print_prompt();
   while (fgets(input_buffer, PATH_MAX, stdin)) {
     line = tokenize(input_buffer);
-
-    // Set redirection variables
-    set_redirection_variables(line);
-    // If the line is empty, continue
-    if (line == NULL) {
+    set_redirection_variables(line); // Set redirection variables
+    bad_command = check_if_all_commands_are_valid(line);
+    if (bad_command != NULL) {
+      print_color(error_file, RED, "msh: command not found: ");
+      print_color(error_file, RED, bad_command);
+      print_color(error_file, RED, "\n");
       print_prompt();
       continue;
     }
-    // If the line is a comment, continue
-    if (line->commands[0].filename[0] == '#') {
-      print_prompt();
-      continue;
-    }
-    // If there is more than one command, communicate them with a pipe, unless "cd" or "exit" is one of them
     if (line->ncommands > 1) {
       if(cd_or_exit_are_present(line)) {
-        print_color(error_file, RED, "msh: cd and exit cannot be used with pipes");
+        print_color(error_file, RED, "msh: cd and exit cannot be used with pipes\n");
         print_prompt();
         continue;
       }
-      else {
-        execute_commands(line->commands);
+      execute_commands(line->commands, line->background);
+    } else {
+      if (strcmp(line->commands[0].argv[0], "cd") == 0) {
+        cd(line->commands[0].argv[1]);
+      } else if (strcmp(line->commands[0].argv[0], "umask") == 0) {
+        _umask(line->commands[0].argv[1]);
+      } else if (strcmp(line->commands[0].argv[0], "fg") == 0) {
+        foreground(atoi(line->commands[0].argv[1]));
+      } else if (strcmp(line->commands[0].argv[0], "jobs") == 0) {
+        jobs();
+      } else if (strcmp(line->commands[0].argv[0], "exit") == 0) {
+        exit(0);
+      } else {
+        execvp(line->commands[0].argv[0], line->commands[0].argv);
       }
     }
     close_redirection_files();
@@ -105,7 +112,7 @@ int main(int argc, char *argv[]) {
 
 // Shell prompt helpers 
 
-/** Print the msh> prompt */
+/** Print the msh> prompt. */
 void print_prompt() {
   char prompt[PATH_MAX], 
        hostname[HOST_NAME_MAX];
@@ -143,7 +150,7 @@ char * polite_directory_format(char * name) {
 
 // Signal handlers
 
-/** Handles SIGINT: ignore, flush and print prompt */ 
+/** Handles SIGINT: ignore, flush and print prompt. */ 
 void sigint_handler(int sig) {
   fflush(stdin);
   print_prompt();
@@ -156,15 +163,16 @@ void sigchld_handler(int sig) {
 
   while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
     if (WIFEXITED(status)) {
-      printf("Process %d exited with status %d", pid, WEXITSTATUS(status));
+      printf("Process %d exited with status %d\n", pid, WEXITSTATUS(status));
     } else if (WIFSIGNALED(status)) {
-      printf("Process %d killed by signal %d", pid, WTERMSIG(status));
+      printf("Process %d killed by signal %d\n", pid, WTERMSIG(status));
     } else if (WIFSTOPPED(status)) {
-      printf("Process %d stopped by signal %d", pid, WSTOPSIG(status));
+      printf("Process %d stopped by signal %d\n", pid, WSTOPSIG(status));
     } else if (WIFCONTINUED(status)) {
-      printf("Process %d continued", pid);
+      printf("Process %d continued\n", pid);
     }
   }
+  signal(SIGCHLD, sigchld_handler);
 }
 
 // Shell helpers
@@ -191,6 +199,7 @@ bool set_redirection_variables(tline * line) {
   input_file = stdin;
   output_file = stdout;
   error_file = stderr;
+  // This code is tedious and repetitive. It needs to be moved to a function.
   if (line->redirect_input) {
     file = fopen(line->redirect_input, "r");
     if (file == NULL) {
@@ -222,16 +231,26 @@ bool set_redirection_variables(tline * line) {
     error_file = file;
   }
 }
-/**  Determines if the line contains the commands "cd" or "exit" */
+/** Determines if any of the commands entered is not valid. */ 
+char * check_if_all_commands_are_valid(tline * line) {
+  if (line == NULL) return NULL;
+  for (size_t i = 0; i < line->ncommands; i++) {
+    if (line->commands[i].filename == NULL) {
+      return line->commands[i].argv[0];
+    }
+  }
+  return NULL;
+}
+/**  Determines if the line contains the commands "cd" or "exit". */
 bool cd_or_exit_are_present(tline * line) {
   for (size_t i = 0; i < line->ncommands; i++) {
-    if (strcmp(line->commands[i].filename, "cd") == 0 || strcmp(line->commands[i].filename, "exit") == 0) {
+    if (strcmp(line->commands[i].argv[0], "cd") == 0 || strcmp(line->commands[i].argv[0], "exit") == 0) {
       return true;
     }
   }
   return false;
 }
-/** Parse a tcommand to string to display it when jobs() is called */ 
+/** Parse a tcommand to string to display it when jobs() is called. */ 
 void parse_tcommand_to_string(tcommand * command, char * buffer) {
   size_t length; 
 
@@ -246,7 +265,45 @@ void parse_tcommand_to_string(tcommand * command, char * buffer) {
     strcat(buffer, command->argv[i]);
   }
 }
-/** Close redirection files if they're not stdin, stdout or stderr */
+/** Execute the commands */
+void execute_commands(tcommand ** commands, bool background) {
+  pid_t pid;
+  int status;
+  char ** argv;
+
+  argv = (char **) malloc((commands[0]->argc) * sizeof(char *));
+  for (size_t i = 0; i < commands[0]->argc - 1; i++) {
+    argv[i] = commands[0]->argv[i + 1];
+  }
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (background) {
+      setpgid(0, 0);
+    }
+    if (execvp(commands[0]->filename, argv) == -1) {
+      print_color(error_file, RED, commands[0]->filename);
+      print_color(error_file, RED, ": ");
+      print_color(error_file, RED, strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+  } else if (pid < 0) {
+    // Error forking
+    print_color(error_file, RED, "fork(): no se pudo crear un nuevo proceso");
+    exit(EXIT_FAILURE);
+  } else {
+    // Parent process
+    if (background) {
+      background_jobs[background_jobs_idx].pid = pid;
+      parse_tcommand_to_string(commands[0], background_jobs[background_jobs_idx].command);
+      background_jobs_idx++;
+    } else {
+      waitpid(pid, &status, 0);
+    }
+  }
+}
+/** Close redirection files if they're not stdin, stdout or stderr. */
 void close_redirection_files() {
   if (input_file != stdin) {
     fclose(input_file);
@@ -260,8 +317,7 @@ void close_redirection_files() {
 }
 
 // Shell commands
-
-/** Change directory */
+/** Change directory. */
 void cd(char * path) {
   if (path == NULL) {
     chdir(getenv("HOME"));
@@ -272,6 +328,27 @@ void cd(char * path) {
 /** Display all active jobs */
 void jobs() {
   for (size_t i = 0; i < background_jobs_idx; i++) {
-    printf("[%d] %s\n", background_jobs[i].pid, background_jobs[i].command);
+    printf("[%d]+  %s\n", i, background_jobs[i].command);
+  }
+}
+/** Set or get the file creation mask of the current process. */
+void _umask (char * mask) {
+  mode_t mask_value;
+  char mask_string[4];
+  char * endptr;
+
+  if (mask != NULL) {
+    mask_value = strtol(mask, &endptr, 8);
+    if (endptr != '\0') {
+      print_color(error_file, RED, "umask: introduzca un número en octal.\n");
+      return;
+    }
+    umask(mask_value);
+    return;
+  } else {
+    mask_value = umask(022);
+    umask(mask_value);
+    sprintf(mask_string, "%03o", mask_value);
+    print_color(output_file, WHITE, mask_string);       
   }
 }
